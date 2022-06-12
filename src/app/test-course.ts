fffcc7ed16d618161a5a2e4ccb63de3bb6b20df3
BrianChanyp <brianchanyp@gmail.com>
2022-06-12 17:00:13 +1200

    Initial commit

---
+++ src/app/test-course.ts
@@ -0,0 +1,41 @@
+import { Course } from './course';
+
+export const COURSES: Course[] = [
+  // ----insert array of five courses with ID here-----
+
+  {
+    course_id: 10,
+    course_title: 'Cloud Computing',
+    semester: 'One',
+    period: 'Tuesday 2-4 PM',
+    lecturer: 'Alan Litchfield',
+  },
+  {
+    course_id: 11,
+    course_title: 'Information Security',
+    semester: 'One',
+    period: 'Wednesday 4-6 PM',
+    lecturer: 'Krassie Petrovva',
+  },
+  {
+    course_id: 12,
+    course_title: 'Operating System',
+    semester: 'One',
+    period: 'Tuesday 9-10 AM',
+    lecturer: 'Edmund Lai',
+  },
+  {
+    course_id: 13,
+    course_title: 'Applied Human Interaction',
+    semester: 'One',
+    period: 'Friday 4-6 PM',
+    lecturer: 'Jim Buchan',
+  },
+  {
+    course_id: 14,
+    course_title: 'Contemp Methods in SE',
+    semester: 'One',
+    period: 'Monday 2-4 PM',
+    lecturer: 'Tony Clear',
+  },
+];
